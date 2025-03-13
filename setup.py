f4c2694fef7c71cac0f3df2e4f5ec6d3be6bba45
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-03-14 00:17:28 +0600

    Added folder Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.1',
+    author= 'Afridi Akbar Ifty',
+    author_email= 'afridiakabrifty@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
